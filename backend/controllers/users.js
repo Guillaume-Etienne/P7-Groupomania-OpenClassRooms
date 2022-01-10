bcf3f732bb3f0df423bd667b0887980489e0e6cf
Gui <gsetienne@free.fr>
2022-01-10 11:43:00 +0200

    backend : proptotype création user sur mySQL fonctionne

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -6,11 +6,16 @@ const dotenv = require("dotenv")
 dotenv.config()
 const MY_APP_SECRET = process.env.APP_SECRET
 
+var schema = new passwordValidator(); //pour définir une niveau minimum de sécurité des mots de passe
+schema
+.is().min(4)                                    // Minimum length 4 (8 nomalement)
 /*
-const dbc = require('../middleware/db-config')
-const db = dbc.getDB()
-
-console.log('coucou : ' + db.query)
+.is().max(100)                                  // Maximum length 100
+.has().uppercase()                              // Must have uppercase letters
+.has().lowercase()                              // Must have lowercase letters
+.has().digits(2)                                // Must have at least 2 digits
+.has().not().spaces()                           // Should not have spaces
+.is().not().oneOf(['Passw0rd', 'Password123']); // Blacklist these values
 */
 
 
@@ -29,30 +34,70 @@ const db = mysql.createConnection({
   database : "p7bdd"
 })
 
-  db.connect(function(err) {
-    if (err) throw err;
-    console.log("Connecté à MySQL chez users.js !");
-  })
+db.connect(function(err) {
+  if (err) throw err;
+  console.log("Connecté à MySQL chez users.js !");
+})
 
 
 
-var schema = new passwordValidator(); //pour définir une niveau minimum de sécurité des mots de passe
-schema
-.is().min(4)                                    // Minimum length 4 (8 nomalement)
-/*
-.is().max(100)                                  // Maximum length 100
-.has().uppercase()                              // Must have uppercase letters
-.has().lowercase()                              // Must have lowercase letters
-.has().digits(2)                                // Must have at least 2 digits
-.has().not().spaces()                           // Should not have spaces
-.is().not().oneOf(['Passw0rd', 'Password123']); // Blacklist these values
-*/
+//création au signup
+
+exports.signup = (req, res, next) => {
+  if (schema.validate(req.body.password)) {
+    bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+        const user = {
+            email : req.body.email,
+            password: hash,
+            name : req.body.name,
+            picture : req.body.picture,
+            moderator : req.body.moderator
+        }
+        user.save()
+            .then(() => res.status(201).json({message : ' Utilisateur  P7 créé !'}))
+            .catch(error => res.status(400).json({ error}))
+    })
+    .catch(error => res.status(500).json({ error}))
+  }
+  else {    
+    res.status(403).json({ message : ' Le mot de passe choisi nest pas assez sécurisé, il doit avoir :' + schema.validate(req.body.password, { list: true })}) //va afficher les détails vérifiés du MDP
+  }        
+}
+
+// qui a marché
+const newby = {
+  email: 'toto@toto.com',
+  password: 'azerty',
+  name: 'Toto de totocity',
+  picture: 'addresse de l image de toto',
+  moderator : true
+}
+
+db.query('INSERT INTO users SET ?', newby, (error, result, fields) =>{
+  if (error) {
+    console.error('Problème création user :' + error)
+    }
+  const id = result.resultId
+  console.log('result : '+ result)
+}
+)
+
+
+
+
+
+
+
 exports.signup = (req, res, next) => {
   console.log(" signup est lancé !")
   return res.status(401).json({ error: 'Utilisateur non trouvé !' });     
 }
 
 exports.login = (req, res, next) => {
+  console.log("login a été appellé !")
+  return res.status(401).json({ error: 'Utilisateur non trouvé !' })
+  /*
     User.findOne({ email: req.body.email })
       .then(user => {
         if (!user) {
@@ -75,4 +120,5 @@ exports.login = (req, res, next) => {
           .catch(error => res.status(500).json({ error }));
       })
       .catch(error => res.status(500).json({ error }));
+      */
   };
