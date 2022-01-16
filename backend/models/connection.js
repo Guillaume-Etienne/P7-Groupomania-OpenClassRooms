868e3cec19956fe0a66f2b16e0c13455f46c29a6
Gui <gsetienne@free.fr>
2022-01-16 21:06:09 +0200

    back : securisation .ENV : OK

--- backend/models/connection.js
+++ backend/models/connection.js
@@ -2,15 +2,18 @@
 
 const mysql = require('mysql')
 const MYSQL_SECRET = process.env.MYSQL_SECRET
+const HOST_SECRET = process.env.HOST_SECRET
+const USER_SECRET = process.env.USER_SECRET
+const DATABASE_SECRET = process.env.DATABASE_SECRET
 
 //Appeler et paramètrer la BDD mySQL
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
+    host: HOST_SECRET,
+    user: USER_SECRET,
     password: MYSQL_SECRET,
     insecureAuth : true,
-    database : "p7bdd"
+    database : DATABASE_SECRET
   })
   
   db.connect(function(err) {
